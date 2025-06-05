29a0506a028c8b342c0da8ee61e47323aa6b3884
Arnon Keereena <arnon.keereena@accenture.com>
2025-06-05 09:54:25 +0700

    - Adjust refactor key mapping for overseer - Align to standard for key mapping for avante (using the ALT + Numbers)

--- lua/core/keymaps/overseer.lua
+++ lua/core/keymaps/overseer.lua
@@ -1,17 +1,35 @@
 local keymap = vim.keymap.set
 
 return function(bufnr)
+	local toggle_command = "<Cmd>OverseerToggle<CR>"
 	-- Overseer uses
-	keymap({ "n", "v", "i" }, "<leader>r", "<Esc><Cmd>OverseerRun<CR>", { desc = "Run Overseer Task", silent = true })
+	keymap(
+		{ "n", "v", "i" },
+		"<leader>r",
+		"<Esc><Cmd>OverseerRun<CR>" .. toggle_command,
+		{ desc = "Run Overseer Task", silent = true }
+	)
+	keymap(
+		{ "n", "v", "i" },
+		"<leader>rr",
+		"<Esc><Cmd>OverseerQuickAction restart<CR>" .. toggle_command,
+		{ desc = "Run Overseer Task", silent = true }
+	)
 	-- ª = <M-9>
-	keymap({ "n", "v", "i" }, "<M-9>", "<Esc><Cmd>OverseerToggle<CR>", { desc = "Toggle Overseer", silent = true })
+	keymap({ "n", "v", "i" }, "<M-9>", "<Esc>" .. toggle_command, { desc = "Toggle Overseer", silent = true })
 	keymap(
 		{ "n", "v", "i" },
 		"<M-.>",
 		"<Esc><Cmd>OverseerQuickAction stop<CR>",
 		{ desc = "Stop last run overseer tasks", silent = true }
 	)
-	keymap({ "t" }, "<M-9>", "<C-\\><C-n><Cmd>OverseerToggle<CR>", { desc = "Toggle Overseer", silent = true })
+	keymap({ "t" }, "<M-9>", "<C-\\><C-n>" .. toggle_command, { desc = "Toggle Overseer", silent = true })
+	keymap(
+		{ "t" },
+		"<leader>rr",
+		"<C-\\><C-n><Cmd>OverseerQuickAction restart<CR>" .. toggle_command,
+		{ desc = "Run Overseer Task", silent = true }
+	)
 	keymap(
 		{ "t" },
 		"<M-.>",
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -46,10 +46,11 @@ return {
 		})
 		local keymap = vim.keymap.set
 
-		keymap("n", "<leader>ac", ":AvanteChat<CR>", { desc = "Avante Chat" })
-		keymap("v", "<leader>ae", ":AvanteEdit<CR>", { desc = "Avante Edit" })
-		keymap("n", "<leader>at", ":AvanteToggle<CR>", { desc = "Avante Toggle" })
-		keymap("n", "<leader>aa", ":AvanteAsk<CR>", { desc = "Avante Ask" })
+		keymap("n", "<leader>ac", "<Esc><Cmd>AvanteChat<CR>", { desc = "Avante Chat" })
+		keymap("v", "<leader>ae", "<Esc><Cmd>AvanteEdit<CR>", { desc = "Avante Edit" })
+		keymap("n", "<leader>at", "<Esc><Cmd>AvanteToggle<CR>", { desc = "Avante Toggle" })
+		keymap("n", "<M-3>", "<Esc><Cmd>AvanteToggle<CR>", { desc = "Avante Toggle" })
+		keymap("n", "<leader>aa", "<Esc><Cmd>AvanteAsk<CR>", { desc = "Avante Ask" })
 	end,
 	build = "make",
 	event = "VeryLazy",
