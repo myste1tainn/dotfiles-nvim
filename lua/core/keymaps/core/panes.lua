fe33ff93e6e22a30d0966ce78e295d70c2b48bf4
Arnon Keereena <arnon.keereena@accenture.com>
2025-06-15 23:03:30 +0700

    - Make opening in float smarter, following options from previous buf /   win, untested

--- lua/core/keymaps/core/panes.lua
+++ lua/core/keymaps/core/panes.lua
@@ -8,8 +8,33 @@ return function(bufnr)
 		{ desc = "Toggle file explorer (neotree)", silent = true }
 	)
 
+	local function get_all_buf_options(bufnr)
+		local opts = {}
+		for _, opt in ipairs(vim.api.nvim_get_all_options_info()) do
+			local name = opt
+			local ok, val = pcall(vim.api.nvim_buf_get_option, bufnr, name)
+			if ok then
+				opts[name] = val
+			end
+		end
+		return opts
+	end
+
+	local function get_win_options(winid)
+		local opts = {}
+		for k, _ in pairs(vim.wo) do
+			local ok, val = pcall(vim.api.nvim_win_get_option, winid, k)
+			if ok then
+				opts[k] = val
+			end
+		end
+		return opts
+	end
+
 	local open_current_buffer_in_float = function()
 		local buf = vim.api.nvim_get_current_buf()
+		local buf_options = get_all_buf_options(buf)
+		local win_options = get_win_options(vim.api.nvim_get_current_win())
 		local width = math.floor(vim.o.columns * 0.9)
 		local height = math.floor(vim.o.lines * 0.9)
 		local row = math.floor((vim.o.lines - height) / 2)
@@ -23,9 +48,12 @@ return function(bufnr)
 			style = "minimal",
 			border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
 		})
-		vim.api.nvimwin_set_option(win, "number", true)
-		vim.api.nvim_win_set_option(win, "relativenumber", true)
-		vim.api.nvim_buf_set_option(buf, "modifiable", true)
+		for option, value in pairs(win_options) do
+			vim.api.nvim_win_set_option(win, option, value)
+		end
+		for option, value in pairs(buf_options) do
+			vim.api.nvim_buf_set_option(buf, option, value)
+		end
 	end
 
 	keymap_util.map_for_all_and_terminal(
