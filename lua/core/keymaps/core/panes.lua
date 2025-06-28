553d5e3c998ab0767ccf0f98ab4e954ab9699e34
Arnon Keereena <arnon.keereena@accenture.com>
2025-06-29 00:47:06 +0700

    - Reconfigure keymaps for toggling panes, to the one that make more   sense in groups

--- lua/core/keymaps/core/panes.lua
+++ lua/core/keymaps/core/panes.lua
@@ -1,64 +1,51 @@
 local keymap_util = require("utils.keymap")
+local panes_util = require("utils.panes")
 
 return function(bufnr)
-	keymap_util.map_for_all_and_terminal("<M-0>", "<Cmd>NoiceAll<CR>", { desc = "Open NoiceAll", silent = true })
-	keymap_util.map_for_all_and_terminal(
-		"<M-1>",
-		"<Cmd>Neotree toggle<CR>",
-		{ desc = "Toggle file explorer (neotree)", silent = true }
-	)
-
-	local function get_all_buf_options(bufnr)
-		local opts = {}
-		for _, opt in ipairs(vim.api.nvim_get_all_options_info()) do
-			local name = opt
-			local ok, val = pcall(vim.api.nvim_buf_get_option, bufnr, name)
-			if ok then
-				opts[name] = val
-			end
-		end
-		return opts
-	end
-
-	local function get_win_options(winid)
-		local opts = {}
-		for k, _ in pairs(vim.wo) do
-			local ok, val = pcall(vim.api.nvim_win_get_option, winid, k)
-			if ok then
-				opts[k] = val
-			end
-		end
-		return opts
-	end
-
-	local open_current_buffer_in_float = function()
-		local buf = vim.api.nvim_get_current_buf()
-		local buf_options = get_all_buf_options(buf)
-		local win_options = get_win_options(vim.api.nvim_get_current_win())
-		local width = math.floor(vim.o.columns * 0.9)
-		local height = math.floor(vim.o.lines * 0.9)
-		local row = math.floor((vim.o.lines - height) / 2)
-		local col = math.floor((vim.o.columns - width) / 2)
-		local win = vim.api.nvim_open_win(buf, true, {
-			relative = "editor",
-			width = width,
-			height = height,
-			row = row,
-			col = col,
-			style = "minimal",
-			border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
+	-- keymap_util.map_for_all_and_terminal("<M-0>", "<Cmd>NoiceAll<CR>", { desc = "Open NoiceAll", silent = true })
+	keymap_util.map_for_all_and_terminal("<M-0>", function()
+		panes_util.toggle_pane({
+			pane_open_predicate = {
+				filetype = "noice",
+			},
+			on_pane_is_being_focused = function(win)
+				vim.cmd("NoiceDismiss")
+				vim.cmd("q")
+			end,
+			on_pane_existed = function(win)
+				vim.api.nvim_set_current_win(win.winid)
+			end,
+			on_pane_not_existed = function()
+				vim.cmd("NoiceAll")
+			end,
 		})
-		for option, value in pairs(win_options) do
-			vim.api.nvim_win_set_option(win, option, value)
-		end
-		for option, value in pairs(buf_options) do
-			vim.api.nvim_buf_set_option(buf, option, value)
-		end
-	end
+	end, { desc = "Open NoiceAll", silent = true })
+	-- keymap_util.map_for_all_and_terminal(
+	-- 	"<M-1>",
+	-- 	"<Cmd>Neotree toggle<CR>",
+	-- 	{ desc = "Toggle file explorer (neotree)", silent = true }
+	-- )
+	keymap_util.map_for_all_and_terminal("<M-1>", function()
+		panes_util.toggle_pane({
+			pane_open_predicate = {
+				filetype = "neo-tree",
+			},
+			on_pane_is_being_focused = function(win)
+				vim.cmd("Neotree close")
+			end,
+			on_pane_existed = function(win)
+				vim.api.nvim_set_current_win(win.winid)
+			end,
+			on_pane_not_existed = function()
+				require("trouble").close("symbols")
+				vim.cmd("Neotree focus")
+			end,
+		})
+	end, { desc = "Toggle file explorer (neotree)", silent = true })
 
 	keymap_util.map_for_all_and_terminal(
-		"<M-f>",
-		open_current_buffer_in_float,
+		"<M-r>",
+		panes_util.open_current_buffer_in_float,
 		{ desc = "Open Buffer in Floating Window", silent = true }
 	)
 end
--- lua/core/keymaps/core/quickfix.lua
+++ lua/core/keymaps/core/quickfix.lua
@@ -1,28 +1,113 @@
 local keymap = vim.keymap.set
+local panes_util = require("utils.panes")
 
-return function(bufnr)
-	-- Toggling Quickfix
-	keymap({ "n", "v", "i" }, "<C-c>", function()
-		local quickfix_open = false
-		for _, win in ipairs(vim.fn.getwininfo()) do
-			if win.quickfix == 1 then
-				quickfix_open = true
-				if win.tabnr == vim.fn.tabpagenr() then
-					if vim.api.nvim_get_current_win() == win.winid then
-						vim.cmd("cclose")
-					else
-						vim.api.nvim_set_current_win(win.winid)
-					end
-					return
-				end
+local function resize_quickfix(win)
+	if vim.api.nvim_win_is_valid(win) then
+		vim.api.nvim_set_current_win(win)
+		vim.cmd("wincmd J")
+		vim.cmd("resize 15")
+	end
+end
+
+-- Always open quickfix window at the bottom
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function(args)
+		if vim.api.nvim_buf_get_option(args.buf, "buftype") == "quickfix" then
+			vim.schedule(function()
+				resize_quickfix(vim.fn.win_getid())
+			end)
+		end
+	end,
+})
+
+-- Reposition quickfix without stealing focus
+vim.api.nvim_create_autocmd({ "WinNew", "WinEnter", "WinResized" }, {
+	callback = function()
+		local current = vim.api.nvim_get_current_win()
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			if vim.api.nvim_buf_get_option(buf, "buftype") == "quickfix" then
+				resize_quickfix(win)
+				-- Return to where you were
+				vim.api.nvim_set_current_win(current)
+				break
 			end
 		end
-		if not quickfix_open then
-			vim.cmd("copen")
-		else
-			vim.cmd("copen")
-		end
+	end,
+})
+
+return function(bufnr)
+	local trouble = require("trouble")
+	-- Toggling Quickfix
+	keymap({ "n", "v", "i" }, "<M-4>", function()
+		-- keymap({ "n", "v", "i" }, "<C-c>", function()
+		panes_util.toggle_pane({
+			pane_open_predicate = {
+				customtype = function(win)
+					local w = vim.w[win.winid].trouble
+					if w == nil then
+						return false
+					end
+					return w.mode == "quickfix"
+				end,
+			},
+			on_pane_is_being_focused = function(win)
+				trouble.close("quickfix")
+			end,
+			on_pane_existed = function(win)
+				---@diagnostic disable-next-line: missing-fields
+				trouble.focus({ mode = "quickfix" }, {})
+			end,
+			on_pane_not_existed = function()
+				trouble.close("diagnostics")
+				trouble.open({ mode = "quickfix", focus = true, win = { position = "bottom" } })
+			end,
+		})
 	end, { desc = "Toggle Quickfix" })
+	keymap({ "n", "v", "i" }, "<M-3>", function()
+		-- keymap({ "n", "v", "i" }, "<C-c>", function()
+		panes_util.toggle_pane({
+			pane_open_predicate = {
+				customtype = function(win)
+					local w = vim.w[win.winid].trouble
+					if w == nil then
+						return false
+					end
+					return w.mode == "diagnostics"
+				end,
+			},
+			on_pane_is_being_focused = function(win)
+				trouble.close("diagnostics")
+			end,
+			on_pane_existed = function(win)
+				---@diagnostic disable-next-line: missing-fields
+				trouble.focus({ mode = "diagnostics" }, {})
+			end,
+			on_pane_not_existed = function()
+				trouble.close("quickfix")
+				trouble.open({ mode = "diagnostics", focus = true, win = { position = "bottom" } })
+			end,
+		})
+	end, { desc = "Toggle Quickfix" })
+	-- keymap({ "n", "v", "i" }, "<M-3>", function()
+	-- 	-- keymap({ "n", "v", "i" }, "<C-c>", function()
+	-- 	panes_util.toggle_pane({
+	-- 		pane_open_predicate = {
+	-- 			customtype = function(win)
+	-- 				return win.quickfix == 1
+	-- 			end,
+	-- 		},
+	-- 		on_pane_is_being_focused = function(win)
+	-- 			vim.cmd("cclose")
+	-- 		end,
+	-- 		on_pane_existed = function(win)
+	-- 			vim.api.nvim_set_current_win(win.winid)
+	-- 		end,
+	-- 		on_pane_not_existed = function()
+	-- 			vim.cmd("copen")
+	-- 		end,
+	-- 	})
+	-- end, { desc = "Toggle Quickfix" })
 
 	-- Navigating between quickfix list
 	keymap({ "n", "v", "i" }, "<C-.>", function()
