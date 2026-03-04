9e0a4193268885ceb5a186492143230661190c72
Arnon Keereena <arnon.keereena@accenture.com>
2026-03-04 12:19:47 +0700

    Add `TelescopeResults` and `noice` to ignore types of the LSP server setup

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -50,7 +50,8 @@ end
 
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(ev)
-		local exlude_filetypes = { "markdown", "text", "gitcommit", "notify", "toggleterm", "TelescopePrompt" }
+		local exlude_filetypes =
+			{ "markdown", "text", "gitcommit", "notify", "toggleterm", "noice", "TelescopeResults", "TelescopePrompt" }
 		if vim.tbl_contains(exlude_filetypes, vim.bo.filetype) then
 			return
 		end
